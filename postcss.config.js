bf75562f00905edc269d210d7f726df52a8ecb14
zhoriyal <zhoriaf2@gmail.com>
2026-04-01 13:06:05 +0700

    Upload pertama halaman profil Zhoriyal

---
+++ postcss.config.js
@@ -0,0 +1,6 @@
+export default {
+  plugins: {
+    '@tailwindcss/postcss': {}, /* <-- Bagian ini yang diubah */
+    autoprefixer: {},
+  },
+}
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // <-- BARIS INI WAJIB ADA
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
